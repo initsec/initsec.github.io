208bf7fa2074a0fbfe1785e0fcbf66095dff826a
Daniel Romero <infoslack@gmail.com>
2013-12-10 22:08:16 -0300

    add new page

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function supportsSVG() {
+    return !! document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect;
+}
+if (!supportsSVG()) {
+    var imgs = document.getElementsByTagName('img');
+    var dotSVG = /.*\.svg$/;
+    for (var i = 0; i != imgs.length; ++i) {
+        if(imgs[i].src.match(dotSVG)) {
+            imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+        }
+    }
+}
+$('.nav').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollOffset: 115,
+    scrollThreshold: 0.5,
+    filter: '',
+});
